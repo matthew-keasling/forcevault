f2b2b8f717a8ea20e603a55e02b9f55adf23b0b8
Matthew Keasling <matthew.keasling@slalom.com>
2016-12-30 15:37:32 -0800

    fixing index route

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ passport.use(new LocalStrategy({
     usernameField: 'email'
   },
   function(username, password, done){
+    console.log(username, password);
     return done(null, {});
   }
 ));
@@ -47,15 +48,17 @@ app.use(function(req, res, next){
 });
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index'));
+// set up URL routes
+var routes = {};
 util.getFiles('./routes').forEach(file => {
-  if(file=='routes/index.js') return;
   var routeName = './'+file;
   var route = require(routeName);
   var routePath = '/'+file.replace(/routes\/(.+)\.\w+?$/,'$1');
+  if(file=='routes/index.js') routePath = '/';
   app.use(routePath, route);
-  console.log('Dynamic route: ' + routePath + ' => ' + routeName);
+  routes[routePath] = routeName;
 });
+console.log('Routes:',JSON.stringify(routes,null,2));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
